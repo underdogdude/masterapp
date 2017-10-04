f5ebfeb630a108cd1972bed14be576a809f46233
PusitK <pk@whitelineactivation.com>
2017-10-04 18:34:07 +0700

    add register

---
+++ js/login.js
@@ -0,0 +1,58 @@
+var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+let form_elem = $('#form_login');
+let img_elem = $('#img_login');
+const view = {
+
+    goToRegister() {
+
+        
+
+        form_elem.addClass('animated-fast ' + 'slideOutLeft').one(animationEnd, function() {
+
+            form_elem.removeClass('animated-fast ' + 'slideOutLeft');
+        });
+        img_elem.addClass('animated-fast ' + 'slideOutRight').one(animationEnd, function() {
+
+            img_elem.removeClass('animated-fast ' + 'slideOutRight');
+            $('#section_login').hide();
+        });
+    },
+
+    goToLogin() {
+        
+        $('#form_register').addClass('animated-fast ' + 'slideOutLeft').one(animationEnd, function() {
+
+            $('#form_register').removeClass('animated-fast ' + 'slideOutLeft');
+        });
+        $('#img_register').addClass('animated-fast ' + 'slideOutRight').one(animationEnd, function() {
+            
+            $('#img_register').removeClass('animated-fast ' + 'slideOutRight');
+
+            $('#section_register').hide();
+            $('#section_login').show();
+        });
+    },
+}
+
+
+
+$('#formLogin').submit(function (e) {
+    e.preventDefault();
+    //code goes here
+    let id = $('#login_id').val();
+    let pass = $('#login_password').val()
+    
+    if(id === 'test' && pass === 'test') {
+
+        let res = document.URL.replace("login", "index");
+              window.open(res,"_self");
+    }else {
+
+        form_elem.addClass('animated-fast ' + 'shake').one(animationEnd, function() {
+
+            form_elem.removeClass('animated-fast ' + 'shake');
+        });
+        $('#alert').show();
+    }
+});
--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 
+
 const view = {
 
     changeModalState() {
-       
         $('#myModal').modal('hide');
         $('#myModal2').modal('show');
-    },
-
+    }
 }
-
-
 
 const logout = () => {
     
     let res = document.URL.replace("index", "login");
-              window.open(res, '_blank');
+              window.open(res, '_self');
 };
